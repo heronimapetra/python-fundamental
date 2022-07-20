fcdbbe569996c1b327a7769a16ea2d733407008a
rinigadjo <rinigadjo@gmail.com>
2022-07-20 08:57:36 +0800

    Sequential and branching

---
+++ dasar_sintaksis_joke_programmer.py
@@ -0,0 +1,69 @@
+"""
+saya mau belajar dasar=dasar python yaitu :
+1. sekuensial,
+2. percabangan dan ;
+3. perulangan
+
+"""
+print("*" * 100)
+
+# Sekuensial
+
+print('Ibu berkata, "Pergilah ke toko dan beli 1 botol susu!"')
+print('Ibu melanjutkan,"Apabila di toko ada telur, belilah juga 6 butir telur."')
+print('Anak menjawab,"Ok".')
+print("Anak pergi ke toko.")
+print('Sesampai di toko anak bertanya,"Apakah ada susu?"')
+print('penjaga toko menjawab,"Susu ada."')
+print("Anak mengecek harganya dan uangnya cukup.")
+print('Anak bertanya lagi,"Apakah ada telur??"')
+print('penjaga toko menjawab,"telur ada."')
+print("Anak membeli 1 botol susu dan 6 butir telur")
+print("Anak pulang ke rumah.")
+print("Anak menyerahkan 1 botol susu dan 6 butir telur ke ibunya.")
+
+print("*" * 100)
+
+# Percabangan
+
+Jumlah_botol_susu = 173
+Jumlah_telur = 1583
+
+print(f"Jumlah botol susu {Jumlah_botol_susu} botol")
+print(f"Jumlah Telur {Jumlah_telur} butir")
+
+if Jumlah_botol_susu > 0:
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu ")
+
+print("Budi pulang ke rumah")
+print("menyampaikan hasilnya kepada ibu")
+
+print("*" * 100)
+
+#Tugas Percabangan
+
+jumlah_bot_susu = 165
+jumlah_tlr = 128
+
+print(f"Jumlah botol susu adalah {jumlah_bot_susu} botol ")
+print(f"Jumlah telur adalah {jumlah_tlr} butir")
+
+if jumlah_bot_susu > 0:
+    print ("Budi membeli 1 botol susu")
+if jumlah_tlr > 6:
+    print("Budi membeli 6 butir telur")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang ke rumah")
+print("menyampaikan hasilnya kepada ibu")
+
+print("*" * 100)
+
+
+
+
+
+
